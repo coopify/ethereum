8a2b5f5f2885db8dc162be22a0aa5521a8ad2ea7
Camilo <camilo.nicolas.martinez@gmail.com>
2019-05-07 23:38:13 -0300

    Added pusher

--- config/index.ts
+++ config/index.ts
@@ -3,12 +3,14 @@ import { ServerConfigs } from './server'
 import { RDBConfigs } from './rdb'
 import { RedisConfigs } from './redis'
 import { Web3Configs } from './web3'
+import { PusherConfigs } from './pusher'
 
 const wLogger = new LoggerConfigs()
 const server = new ServerConfigs()
 const redis = new RedisConfigs()
 const rdb = new RDBConfigs()
 const web3 = new Web3Configs()
+const pusher = new PusherConfigs()
 
 export function validateAll() {
     return [
@@ -17,7 +19,8 @@ export function validateAll() {
         rdb.validate(),
         redis.validate(),
         web3.validate(),
+        pusher.validate(),
       ].filter((x) => x.hasErrors)
 }
 
-export { wLogger, server, redis, rdb, web3 }
+export { wLogger, server, redis, rdb, web3, pusher }
