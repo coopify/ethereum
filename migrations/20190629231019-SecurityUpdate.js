f3eff1d3eee5b7096550d520c17e241081727ddf
Camilo <camilo.nicolas.martinez@gmail.com>
2019-08-01 20:07:14 -0300

    Fixes to sec migration

--- migrations/20190629231019-SecurityUpdate.js
+++ migrations/20190629231019-SecurityUpdate.js
@@ -3,35 +3,35 @@ const config = require('../dist/config')
 const services = require('../dist/src/server/services')
 
 module.exports = {
-    up: async (queryInterface) => {
-        await config.validateAll()
-        await services.initWLogger()
-        await services.initExternalServices()
-        const UserModel = queryInterface.sequelize.define('User', await queryInterface.describeTable('User'), { tableName: 'User' })
-        const existingUsers = await UserModel.findAll({})
-        return Promise.all(
-            existingUsers.map(async user => {
-                const oldData = user.pk
-                const newData = services.encryption.encryptString(oldData)
-                Object.assign(user, { ...user, pk: newData })
-                return user.save()
+    up: (queryInterface) => {
+        config.validateAll()
+        services.initExternalServices()
+        return queryInterface.describeTable('User').then((table) => {
+            const UserModel = queryInterface.sequelize.define('User', table, { tableName: 'User' })
+            UserModel.findAll({}).then((users) => {
+                users.map((user) => {
+                    const oldData = user.pk
+                    const newData = services.encryption.encryptString(oldData)
+                    Object.assign(user, { ...user, pk: newData })
+                    return user.save()
+                })
             })
-        )
+        })
     },
 
     down: (queryInterface) => {
-        await config.validateAll()
-        await services.initWLogger()
-        await services.initExternalServices()
-        const UserModel = queryInterface.sequelize.define('User', await queryInterface.describeTable('User'), { tableName: 'User' })
-        const existingUsers = await UserModel.findAll({})
-        return Promise.all(
-            existingUsers.map(async user => {
-                const oldData = user.pk
-                const newData = services.encryption.decryptString(oldData)
-                Object.assign(user, { ...user, pk: newData })
-                return user.save()
+        config.validateAll()
+        services.initExternalServices()
+        return queryInterface.describeTable('User').then((table) => {
+            const UserModel = queryInterface.sequelize.define('User', table, { tableName: 'User' })
+            UserModel.findAll({}).then((users) => {
+                users.map((user) => {
+                    const oldData = user.pk
+                    const newData = services.encryption.encryptString(oldData)
+                    Object.assign(user, { ...user, pk: newData })
+                    return user.save()
+                })
             })
-        )
+        })
     }
 };
